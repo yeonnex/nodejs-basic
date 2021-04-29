369b8f83d71b6b5ca6d947b544bd5afb53c083fa
yeonnex <yeonnex@gmail.com>
2021-04-29 18:26:12 +0900

    전역변수부터 모듈 임포트까지

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 12
+
+if (amount > 10){
+    console.log('large number')
+}
+else{
+    console.log('small number')
+}
+
+console.log(`this is my first node app!!!`)
---
+++ 4-names.js
@@ -0,0 +1,10 @@
+// local
+const secret = 'SUPER SECRET'
+//share
+const john = 'john'
+const peter = 'peter'
+
+console.log(module)
+
+// export multiple value
+module.exports = {john,peter} 
