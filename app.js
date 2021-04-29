e7eee5626c039d781281ded27f8a90bc5fde64c4
yeonnex <yeonnex@gmail.com>
2021-04-29 22:26:45 +0900

    node 모듈 install

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const http = require('http')
-const server= http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.end('Welcome to our home page!')
-    }
-    if(req.url === '/about'){
-        res.end('Here is our short history')
-    }
-    
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem tm find th page</p>
-    <a href="/">back home</a>
-        `
-    )
-})
+// npm init -y (everything default)
 
-server.listen(5000)
+const _ = require('lodash')
+
+const items = [1, [2,[3,[4]]]]
+
+const newItems = _.flattenDeep(items)
+console.log(newItems)
