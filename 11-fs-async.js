e2d9c90b042106976a0ad1a2cabe5635a9a2f70c
yeonnex <yeonnex@gmail.com>
2021-04-29 21:59:52 +0900

    http 모듈과 url 라우팅

---
+++ 11-fs-async.js
@@ -0,0 +1,29 @@
+const { readFile, writeFile } = require("fs");
+
+// 콜백헬
+
+readFile('./content/first.txt','utf-8', (err, result)=>{
+    if (err){
+        console.log(err)
+        return
+    }
+    const first = result
+    readFile('./content/second.txt', 'utf-8', (err, result)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}`,
+            (err, result)=>{
+                if (err){
+                    console.log(err)
+                    return
+                }
+                console.log(result)
+            }
+        )
+    })
+})
